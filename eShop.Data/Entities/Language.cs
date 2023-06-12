2730c6c08a5871f550b82bedb6c46f0e4bd3bf08
venus2410 <nguyencongdanh2410@gmail.com>
2023-06-12 19:54:26 +0700

    create class of database

---
+++ eShop.Data/Entities/Language.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Data.Entities
+{
+    public class Language
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
