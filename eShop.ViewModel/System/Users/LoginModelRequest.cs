b2eb3c531fb3fff020828e4e24746460e271f6b6
venus2410 <nguyencongdanh2410@gmail.com>
2023-06-21 08:28:19 +0700

    add login and register

---
+++ eShop.ViewModel/System/Users/LoginModelRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.ViewModel.System.Users
+{
+    public class LoginModelRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
