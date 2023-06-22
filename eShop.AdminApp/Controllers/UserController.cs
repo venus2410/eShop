cd6032c1362fb8091ef1c0e8052243584472d4fa
venus2410 <nguyencongdanh2410@gmail.com>
2023-06-22 20:40:49 +0700

    done connect to api

---
+++ eShop.AdminApp/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using eShop.AdminApp.Services;
+using eShop.ViewModel.System.Users;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+
+namespace eShop.AdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserApiClient _userApiClient;
+        public UserController(IUserApiClient userApiClient)
+        {
+              _userApiClient= userApiClient;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModelRequest request)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(ModelState);
+            }
+            var result=await _userApiClient.Login(request);
+            return View();
+        }
+    }
+}
---
+++ eShop.AdminApp/Services/UserApiClient.cs
@@ -0,0 +1,31 @@
+﻿using eShop.ViewModel.System.Users;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace eShop.AdminApp.Services
+{
+    public class UserApiClient : IUserApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public UserApiClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory= httpClientFactory;
+        }
+        public async Task<string> Login(LoginModelRequest model)
+        {
+            var json = JsonConvert.SerializeObject(model);
+            var httpContent= new StringContent(json,Encoding.UTF8, Application.Json);
+
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new System.Uri("https://localhost:44341");
+            var response =await client.PostAsync("/api/users/authenticate",httpContent);
+            var token=await response.Content.ReadAsStringAsync();
+            return token;
+        }
+    }
+}
